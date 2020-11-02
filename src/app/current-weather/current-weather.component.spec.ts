25df5d21bf164200d52be0be9c0954d6943f66fd
links <sergeevvs92@gmail.com>
2020-11-02 16:22:07 +0200

    fixed unit and e2e tests

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -3,6 +3,7 @@ import { By } from '@angular/platform-browser'
 import { injectSpy } from 'angular-unit-test-helper'
 import { of } from 'rxjs'
 
+import { MaterialModule } from '../material.module'
 import { WeatherService } from '../weather/weather.service'
 import { fakeWeather } from '../weather/weather.service.fake'
 import { CurrentWeatherComponent } from './current-weather.component'
@@ -19,6 +20,7 @@ describe('CurrentWeatherComponent', () => {
     ])
     TestBed.configureTestingModule({
       declarations: [CurrentWeatherComponent],
+      imports: [MaterialModule],
       providers: [
         {
           provide: WeatherService,
@@ -71,7 +73,7 @@ describe('CurrentWeatherComponent', () => {
 
     // Assert on DOM
     const debugEl = fixture.debugElement
-    const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement
+    const titleEl: HTMLElement = debugEl.query(By.css('.mat-title')).nativeElement
     expect(titleEl.textContent).toContain('Bethesda')
   })
 })
